e2f971abb2db573d382e1e95dcf44abe0da1b5bf
kmlsd <kmlsd4757@gmail.com>
2023-06-28 13:38:20 -0700

    upd

--- main.h
+++ main.h
@@ -166,15 +166,6 @@ int print_reverse(va_list types, char buffer[],
 
 int flags, int width, int precision, int size);
 
-int get_flags(const char *format, int *i);
-
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-int flags, int width, int precision, int size);
-
 
 
 /*Function to print a string in rot 13*/
